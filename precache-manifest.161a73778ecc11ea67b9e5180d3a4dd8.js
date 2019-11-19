fd48e37185549cc79f58b51fc37122f49e69b27b
Pawel <ssopel10@gmail.com>
2019-11-19 16:17:12 +0100

    Updates

---
+++ precache-manifest.161a73778ecc11ea67b9e5180d3a4dd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddde2f98b3d7102ac9d49b331ce2ade5",
+    "url": "/shop_app/index.html"
+  },
+  {
+    "revision": "6c1905669e60aa09281f",
+    "url": "/shop_app/static/css/main.a9952037.chunk.css"
+  },
+  {
+    "revision": "496db2c95b4d15bb2091",
+    "url": "/shop_app/static/js/2.1bd692b8.chunk.js"
+  },
+  {
+    "revision": "6c1905669e60aa09281f",
+    "url": "/shop_app/static/js/main.1f3d8ec7.chunk.js"
+  },
+  {
+    "revision": "57531def330896fcc921",
+    "url": "/shop_app/static/js/runtime-main.5d0ee9a6.js"
+  }
+]);
